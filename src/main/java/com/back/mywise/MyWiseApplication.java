48247d37fa1945504801d30662111d027044dd07
lcs9317 <l65783082@gmail.com>
2025-06-02 10:25:33 +0900

    mywise

---
+++ src/main/java/com/back/mywise/MyWiseApplication.java
@@ -0,0 +1,13 @@
+package com.back.mywise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/mywise/MyWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.mywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
